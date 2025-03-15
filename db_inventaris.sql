dda9975373d4c2e6c3ab5c65c04b456307821ae6
davidindra22 <daviedindra20@gmail.com>
2025-03-15 14:51:47 +0700

    Add file db_inventaris new

--- db_inventaris.sql
+++ db_inventaris.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Mar 14, 2025 at 09:58 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.0.30
+-- Host: localhost:3306
+-- Generation Time: Mar 15, 2025 at 07:51 AM
+-- Server version: 8.0.30
+-- PHP Version: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -28,12 +28,12 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `mrp` (
-  `id` int(11) NOT NULL,
-  `kode_barang` varchar(8) DEFAULT NULL,
-  `jml_barangkeluar` int(11) DEFAULT NULL,
-  `jml_barangmasuk` int(11) DEFAULT NULL,
-  `safety_stock` int(11) DEFAULT NULL,
-  `net_requirements` int(11) DEFAULT NULL,
+  `id` int NOT NULL,
+  `kode_barang` varchar(8) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `jml_barangkeluar` int DEFAULT NULL,
+  `jml_barangmasuk` int DEFAULT NULL,
+  `safety_stock` int DEFAULT NULL,
+  `net_requirements` int DEFAULT NULL,
   `tanggal_input_mrp` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -42,7 +42,9 @@ CREATE TABLE `mrp` (
 --
 
 INSERT INTO `mrp` (`id`, `kode_barang`, `jml_barangkeluar`, `jml_barangmasuk`, `safety_stock`, `net_requirements`, `tanggal_input_mrp`) VALUES
-(1, 1, 12, 0, 10, 0, '2025-02-22');
+(2, 'MJC301', 28, 355, 327, 10, '2025-03-15'),
+(3, 'MJC301', 43, 355, 312, 15, '2025-03-15'),
+(4, 'MJC304', 50, 50, 0, 50, '2025-03-15');
 
 -- --------------------------------------------------------
 
@@ -53,7 +55,7 @@ INSERT INTO `mrp` (`id`, `kode_barang`, `jml_barangkeluar`, `jml_barangmasuk`, `
 CREATE TABLE `tbl_barang` (
   `kode_barang` varchar(8) NOT NULL,
   `nama_barang` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tbl_barang`
@@ -76,8 +78,8 @@ CREATE TABLE `tbl_keluarbarang` (
   `nama_barang` varchar(30) NOT NULL,
   `tgl_pinjam` date NOT NULL,
   `peminjam` varchar(35) NOT NULL,
-  `jumlah_pinjam` int(8) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+  `jumlah_pinjam` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tbl_keluarbarang`
@@ -107,7 +109,11 @@ INSERT INTO `tbl_keluarbarang` (`no_pinjam`, `kode_barang`, `nama_barang`, `tgl_
 ('', 'MJC301', ' ROCKING CHAIR', '2025-02-22', 'sarah', 10),
 ('', 'MJC301', ' ROCKING CHAIR', '2025-02-22', 'sarah', 10),
 ('', 'MJC301', ' ROCKING CHAIR', '2025-02-22', 'sarah', 10),
-('', 'MJC301', ' ROCKING CHAIR', '2025-02-22', 'sarah', 10);
+('', 'MJC301', ' ROCKING CHAIR', '2025-02-22', 'sarah', 10),
+('PMJN011', 'MJC301', ' ROCKING CHAIR', '2025-03-14', 'deka', 10),
+('PMJN012', 'MJC301', ' ROCKING CHAIR', '2025-03-14', 'deka', 10),
+('PMJN013', 'MJC301', ' ROCKING CHAIR', '2025-03-15', 'aku', 15),
+('PMJN014', 'MJC304', 'STACKING CHAIR', '2025-03-15', 'intan', 50);
 
 -- --------------------------------------------------------
 
@@ -120,9 +126,9 @@ CREATE TABLE `tbl_masukbarang` (
   `kode_barang` varchar(8) NOT NULL,
   `nama_barang` varchar(30) NOT NULL,
   `tgl_masuk` date NOT NULL,
-  `jumlah_masuk` int(8) NOT NULL,
+  `jumlah_masuk` int NOT NULL,
   `kode_supplier` varchar(5) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tbl_masukbarang`
@@ -133,7 +139,11 @@ INSERT INTO `tbl_masukbarang` (`id_masukbarang`, `kode_barang`, `nama_barang`, `
 ('BMSK003', 'MJC304', 'STACKING CHAIR', '2025-02-10', 10, 'SP001'),
 ('BMSK004', 'MJC301', ' ROCKING CHAIR', '2025-02-10', 10, 'SP003'),
 ('BMSK005', 'MJC302', 'RECLINER', '2025-02-10', 12, 'SP003'),
-('BMSK006', 'MJC304', 'STACKING CHAIR', '2025-02-11', 1, 'SP002');
+('BMSK006', 'MJC304', 'STACKING CHAIR', '2025-02-11', 1, 'SP002'),
+('BMSK007', 'MJC302', 'RECLINER', '2025-03-15', 100, 'SP003'),
+('BMSK008', 'MJC302', 'RECLINER', '2025-03-15', 100, 'SP003'),
+('BMSK009', 'MJC302', 'RECLINER', '2025-03-15', 100, 'SP003'),
+('BMSK010', 'MJC304', 'STACKING CHAIR', '2025-03-15', 49, 'SP001');
 
 -- --------------------------------------------------------
 
@@ -146,9 +156,9 @@ CREATE TABLE `tbl_pinjam` (
   `tgl_pinjam` date NOT NULL,
   `kode_barang` varchar(8) NOT NULL,
   `nama_barang` varchar(30) NOT NULL,
-  `jumlah_pinjam` int(7) NOT NULL,
+  `jumlah_pinjam` int NOT NULL,
   `peminjam` varchar(35) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tbl_pinjam`
@@ -163,7 +173,11 @@ INSERT INTO `tbl_pinjam` (`nomor_pinjam`, `tgl_pinjam`, `kode_barang`, `nama_bar
 ('PMJN007', '2025-02-22', 'MJC301', ' ROCKING CHAIR', 10, 'sarah'),
 ('PMJN008', '2025-02-22', 'MJC301', ' ROCKING CHAIR', 10, 'sarah'),
 ('PMJN009', '2025-02-22', 'MJC301', ' ROCKING CHAIR', 10, 'sarah'),
-('PMJN010', '2025-02-22', 'MJC301', ' ROCKING CHAIR', 10, 'sarah');
+('PMJN010', '2025-02-22', 'MJC301', ' ROCKING CHAIR', 10, 'sarah'),
+('PMJN011', '2025-03-14', 'MJC301', ' ROCKING CHAIR', 10, 'deka'),
+('PMJN012', '2025-03-14', 'MJC301', ' ROCKING CHAIR', 10, 'deka'),
+('PMJN013', '2025-03-15', 'MJC301', ' ROCKING CHAIR', 15, 'aku'),
+('PMJN014', '2025-03-15', 'MJC304', 'STACKING CHAIR', 50, 'intan');
 
 -- --------------------------------------------------------
 
@@ -172,21 +186,22 @@ INSERT INTO `tbl_pinjam` (`nomor_pinjam`, `tgl_pinjam`, `kode_barang`, `nama_bar
 --
 
 CREATE TABLE `tbl_stok` (
-  `id_stok` int(11) NOT NULL,
+  `id_stok` int NOT NULL,
   `kode_barang` varchar(8) NOT NULL,
   `nama_barang` varchar(30) NOT NULL,
-  `jml_barangmasuk` int(7) NOT NULL,
-  `jml_barangkeluar` int(7) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+  `jml_barangmasuk` int NOT NULL,
+  `jml_barangkeluar` int NOT NULL,
+  `total_barang` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tbl_stok`
 --
 
-INSERT INTO `tbl_stok` (`id_stok`, `kode_barang`, `nama_barang`, `jml_barangmasuk`, `jml_barangkeluar`) VALUES
-(1, 'MJC302', 'RECLINER', 12, 3),
-(2, 'MJC301', 'ROCKING CHAIR', 355, 18),
-(3, 'MJC304', 'STACKING CHAIR', 1, 0);
+INSERT INTO `tbl_stok` (`id_stok`, `kode_barang`, `nama_barang`, `jml_barangmasuk`, `jml_barangkeluar`, `total_barang`) VALUES
+(1, 'MJC302', 'RECLINER', 212, 3, 209),
+(2, 'MJC301', 'ROCKING CHAIR', 355, 43, 312),
+(3, 'MJC304', 'STACKING CHAIR', 50, 50, 0);
 
 -- --------------------------------------------------------
 
@@ -200,7 +215,7 @@ CREATE TABLE `tbl_supplier` (
   `alamat_supplier` varchar(50) NOT NULL,
   `telp_supplier` varchar(25) NOT NULL,
   `kota_supplier` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci ROW_FORMAT=COMPACT;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
 
 --
 -- Dumping data for table `tbl_supplier`
@@ -221,7 +236,7 @@ CREATE TABLE `tbl_user` (
   `id_user` varchar(8) NOT NULL,
   `username` varchar(35) NOT NULL,
   `password` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tbl_user`
@@ -285,23 +300,13 @@ ALTER TABLE `tbl_user`
 -- AUTO_INCREMENT for table `mrp`
 --
 ALTER TABLE `mrp`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `tbl_stok`
 --
 ALTER TABLE `tbl_stok`
-  MODIFY `id_stok` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `mrp`
---
-ALTER TABLE `mrp`
-  ADD CONSTRAINT `mrp_ibfk_1` FOREIGN KEY (`kode_barang`) REFERENCES `tbl_stok` (`id_stok`);
+  MODIFY `id_stok` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
